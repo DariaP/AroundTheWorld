0f5f6e65094a8f248a25560086785ee99784205c
Daria <dashka.kh@gmail.com>
2014-09-07 18:30:12 -0700

    Added google map

--- webClient/main.js
+++ webClient/main.js
@@ -1,32 +1,44 @@
-            
-function onLoad() {
-  function getDataClient(url, placesCallback, connectionClosedCallback) {
-    var serverUnavailable = function () {
-      $('#places').text("Temporary unavailable");
-      connectionClosedCallback();
-    };
-
-    if (typeof io === 'undefined') {
-      serverUnavailable();
-    }
-
-    var socket = io.connect(url);
-    socket.on('allPlaces', placesCallback);
-    socket.on('disconnect', function() {
-      serverUnavailable();
-    });
-
-    return {
-      requestAllPlaces : function(url) {
-        socket.emit('getAllPlaces');
-      }
-    };
+function initMaps() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8
   };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+  console.log(map)
+};
+function getDataClient(url, placesCallback, connectionClosedCallback) {
+  var serverUnavailable = function () {
+    $('#places').text("Temporary unavailable");
+    connectionClosedCallback();
+  };
+
+  if (typeof io === 'undefined') {
+    serverUnavailable();
+  }
+
+  var socket = io.connect(url);
+  socket.on('allPlaces', placesCallback);
+  socket.on('disconnect', function() {
+    serverUnavailable();
+  });
+
+  return {
+    requestAllPlaces : function(url) {
+      socket.emit('getAllPlaces');
+    }
+  };
+};
+function startDataClient() {
   dataClient = getDataClient('http://localhost:29999',
     function(places) {
-      $('#places').text(JSON.stringify(places));
+      console.log(JSON.stringify(places))
     },
     function() {}
-  );
-  dataClient.requestAllPlaces()
+    );
+  dataClient.requestAllPlaces()  
+};
+function onLoad() {
+  initMaps();
+  startDataClient();
 }
