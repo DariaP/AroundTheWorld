b5b71d2e3f79f073c61c4828e619a188cbe23caf
Daria <dashka.kh@gmail.com>
2014-09-08 05:37:39 -0700

    Added my own css instead of bootstrap

--- webClient/main.js
+++ webClient/main.js
@@ -25,16 +25,17 @@ function getDataClient(url, connectionClosedCallback) {
 };
 
 function onLoad() {
+  $('#details-sidebar').hide();
+
   map = initMap();
   dataClient = getDataClient(
     'http://localhost:29999',
     function() {}
-  );
+    );
   dataClient.onPlaces(function(places) {
-      console.log(JSON.stringify(places))
-      places.forEach(function(place) {
-    map.putPlaceMarker(place);
-});
+    places.forEach(function(place) {
+      map.putPlaceMarker(place);
+    });
   });
   dataClient.requestAllPlaces();
 }
