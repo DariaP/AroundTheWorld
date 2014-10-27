efd2ea7a6cbdf1691e1eaa66b4e6f0cefde93f29
Daria <dashka.kh@gmail.com>
2014-10-27 14:50:45 -0700

    Run all tests or only specified one

--- casper/run.sh
+++ casper/run.sh
@@ -1,7 +1,18 @@
 #!/bin/zsh
 
-node ../../AroundTheWorld-server/initDb.js
-node ../../AroundTheWorld-server/index.js &
-pid=$(ps | grep 'index.js' | grep -v 'grep' | awk '{print $1;}')
-casperjs test mapsSidebarTests.js
-kill $pid
+setopt shwordsplit 
+scripts=
+
+if [ "$1" != "" ]; then
+    scripts="$1"
+else
+    scripts=$(ls | grep '.js')
+fi
+ 
+for script in $scripts; do
+	node ../../AroundTheWorld-server/initDb.js
+	node ../../AroundTheWorld-server/index.js &
+	pid=$(ps | grep 'index.js' | grep -v 'grep' | awk '{print $1;}')
+	casperjs test $script
+	kill $pid
+done
