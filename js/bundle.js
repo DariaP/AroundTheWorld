056b3f6d2add48adac7357581317ffb1985e8184
Daria <dashka.kh@gmail.com>
2014-11-11 12:58:11 -0800

    Don't fetch parent maps if there is no such maps

--- js/bundle.js
+++ js/bundle.js
@@ -113,18 +113,22 @@ var ParentMaps = Backbone.Collection.extend({
   model: Map,
 
   initialize: function(options) {
-    this.url = 'http://localhost:8089/maps?' + 
-      options.ids.map(function(id) {
-        return 'ids=' + id;
-      }).join('&');
+    if(options.ids.length != 0) {
+      this.url = 'http://localhost:8089/maps?' + 
+        options.ids.map(function(id) {
+          return 'ids=' + id;
+        }).join('&');
+    } 
   },
 
   fetch: function(options) {
+    if (this.url) {
     return Backbone.Collection.prototype.fetch.call(this, options).then(null, function(res) {
       if (res.responseJSON && res.responseJSON.err) {
         alert("Can't get parent maps, please try later");
       }
     });
+    }
   }
 });
 
