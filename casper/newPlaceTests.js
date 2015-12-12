3f0055c61c884999fa71ad427ba000835937d352
Daria <dashka.kh@gmail.com>
2015-12-11 20:31:33 -0800

    Add new place tests fixed

--- casper/newPlaceTests.js
+++ casper/newPlaceTests.js
@@ -1,11 +1,53 @@
+var system = require('system');
+
+casper.on("resource.error", function(resourceError){
+});
 
 casper.on('remote.message', function(message) {
   this.echo('remote message caught: ' + message);
 });
 
+function login(test, callback) {
+  casper.start(casper.cli.get("addr"), function() {
+    test.assertTitle("Around the world", "title is the one expected");
+    this.click('a[href="/auth/facebook"]');
+    this.waitForText("Log into Facebook",
+      function pass () {
+        test.assertExists('div#loginform');
+        this.sendKeys('div#loginform input[name = "email"]',  system.env.FACEBOOK_USERNAME);
+        this.sendKeys('div#loginform input[name = "pass"]',  system.env.FACEBOOK_PASSWORD);
+        test.assertExists('#login_button_inline input');
+        this.click('#login_button_inline input');
+
+        this.waitForText("Hello, Daria Protsenko",
+          function pass () { 
+            callback(this);
+          },
+          function fail () {
+            test.fail("Did not load page");
+          }
+        );
+
+      },
+      function fail () {
+        test.fail("Did not load facebook login page");
+      }
+    );
+  });
+}
+
+casper.test.begin("Add new place", 3, function(test) {
+
+  login(test, function() {});
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
 casper.test.begin("Add new place", 8, function(test) {
 
-  casper.start("file:///Users/daria/github/AroundTheWorld/index.html", function() {
+  casper.start(casper.cli.get("addr"), function() {
     this.click('#new-place-tab-nav');
     casper.fill(
       '#new-place-tab form',
@@ -13,7 +55,7 @@ casper.test.begin("Add new place", 8, function(test) {
         'name': 'Center of the World',
         'location': '0, 0',
         'notes': 'center',
-        'pics': 'http://4hdwallpaper.com/wp-content/uploads/2014/10/cell-sea-wallpapers.jpg'
+        'pics': 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Clouds_over_the_Atlantic_Ocean.jpg'
       }, true);
 
     this.click('#my-maps-nav a');
@@ -34,7 +76,7 @@ casper.test.begin("Add new place", 8, function(test) {
     test.assertSelectorHasText('.place-details .place-name', 'Center of the World');
     test.assertSelectorHasText('.place-details .location', '0, 0');
     test.assertSelectorHasText('.place-details .notes', 'center');
-    test.assertElementCount('.place-details .pic-links a', 1);
+    test.assertElementCount('.place-details .all-pics a', 1);
   });
 
   casper.run(function() {
@@ -42,34 +84,25 @@ casper.test.begin("Add new place", 8, function(test) {
   });
 });
 
-casper.test.begin("Edit new place", 5, function(test) {
+casper.test.begin("Edit new place", 3, function(test) {
 
-  casper.start("file:///Users/daria/github/AroundTheWorld/index.html", function() {
+  casper.start(casper.cli.get("addr"), function() {
     this.click('#my-maps-nav a');
     this.click('.maps.sidebar #USA a');
     this.click('.maps.sidebar #Center-of-the-World a');
-    this.click('.place-details .edit');
-  });
 
-  casper.then(function() {
-    casper.fill(
-      '.place-details form',
-      {
-        'name': 'Not really the center',
-        'location': '1, 1',
-        'notes': 'Somewhere close to the center',
-        'pics': ''
-      });
-    this.click('.parent-maps #USA .remove');
-    this.click('.place-details form .save');
-  });
+    this.click('.place-details .place-name .edit');
+    casper.fill('.place-details form', {'name': 'Not really the center' }, true);
+ 
+    this.click('.place-details .notes .edit');
+    casper.fill('.place-details form', {'notes': 'Somewhere close to the center' }, true);
 
-  casper.then(function() {
+    this.click('.place-details .location .edit');
+    casper.fill('.place-details form', {'location': '1, 1' }, true);
+
     test.assertSelectorHasText('.place-details .place-name', 'Not really the center');
-    test.assertSelectorHasText('.place-details .location', '1, 1');
     test.assertSelectorHasText('.place-details .notes', 'Somewhere close to the center');
-    test.assertSelectorDoesntHaveText('.place-details .parent-maps', 'USA');
-    test.assertElementCount('.place-details .pic-links a', 0);
+    test.assertSelectorHasText('.place-details .location', '1, 1');
   });
 
   casper.run(function() {
