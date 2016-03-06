4c16de58ac067f8e258332c79706cd5be09d4bdf
Daria <dashka.kh@gmail.com>
2016-03-06 08:14:28 -0800

    Maps list tests

--- e2e/mapsList.js
+++ e2e/mapsList.js
@@ -1,5 +1,9 @@
 var login = require('./login.js');
 
+var newMapButtonSelector = 'div[ui-view="mapsSidebarContent"] h4 button',
+    newMapInputSelector = 'div[ui-view="mapsSidebarContent"] input',
+    newMapFormSelector = 'div[ui-view="mapsSidebarContent"] form';
+
 describe('Around the world main page', function() {
   it('should show maps list', function() {
     login(browser.driver);
@@ -19,4 +23,28 @@ describe('Around the world main page', function() {
       expect(titles.indexOf('Mountain trails')).not.toEqual(-1);
     });
   });
+
+  it('should add new map', function() {
+
+    element(by.linkText('My maps')).click();
+
+    var maps = element.all(by.repeater('map in maps'));
+    expect(maps.count()).toEqual(4);
+
+    element(by.css(newMapButtonSelector)).click();
+
+    element(by.css(newMapInputSelector)).sendKeys("Test");
+    element(by.css(newMapFormSelector)).submit();
+
+    var maps = element.all(by.repeater('map in maps'));
+    expect(maps.count()).toEqual(5);
+
+    var mapTitles = maps.map(function(map) {
+      return map.getText();
+    });
+    mapTitles.then(function (titles) {
+      expect(titles.indexOf('Test')).not.toEqual(-1);
+    });
+  });
+
 });
--- e2e/run.sh
+++ e2e/run.sh
@@ -1,1 +1,5 @@
+cd ..
+node initDb.js > /dev/null
+cd e2e
+
 protractor --params.login="$FACEBOOK_USERNAME" --params.password="$FACEBOOK_PASSWORD" conf.js --specs "$1"
