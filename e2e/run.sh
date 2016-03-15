c62ad0c91cd36d77e789788aef45591f66f42de5
Daria <dashka.kh@gmail.com>
2016-03-15 14:40:22 -0700

    Run all tests in directory

--- e2e/run.sh
+++ e2e/run.sh
@@ -1,5 +1,26 @@
+#!/bin/zsh
+
+setopt shwordsplit 
+scripts=
+
+if [ "$1" != "" ]; then
+    scripts="$1"
+else
+    scripts=$(ls | grep '.js' | grep -Ev 'conf.js|login.js|aroundTheWorldPage.js')
+fi
+
+for script in $scripts; do
+	cd ..
+	node initDb.js > /dev/null
+
+	cd e2e
+
+	echo "\nRunning test: "
+	echo "$script"
+	echo "\n"
+
+	protractor --params.login="$FACEBOOK_USERNAME" --params.password="$FACEBOOK_PASSWORD" conf.js --specs "$script"
+done
+
 cd ..
 node initDb.js > /dev/null
-cd e2e
-
-protractor --params.login="$FACEBOOK_USERNAME" --params.password="$FACEBOOK_PASSWORD" conf.js --specs "$1"
